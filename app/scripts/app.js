42cf044d16cc44f7fb966c082ee210cf8fce9b7d
Mario Nava <nava.mario@gmail.com>
2015-09-26 01:44:45 -0500

    Creamos el módulo principal de la aplicación y las rutas iniciales

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    angular.module('blog', ['ngRoute', 'blog.controllers']);
+
+    function config ($locationProvider, $routeProvider) {
+        //Permite que las rutas NO lleven el carater # al inicio de ellas
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                templateUrl : 'views/post-list.tpl.html',
+                controller : 'PostListController',
+                controllerAs : 'postlist'
+            })
+            .when('/post/:postId', {
+                templateUrl : 'views/post-detail.tpl.html',
+                controller : 'PostDetailController',
+                controllerAs : 'postdetail'
+            })
+            .when('/new', {
+                templateUrl : 'views/post-create.tpl.html',
+                controllers : 'PostCreateController',
+                controllerAs : 'postcreate'
+            });
+    }
+    //Asociamos la funcion config creada en el módulo
+    angular
+        .module('blog')
+        .config(config);
+
+}) ();
