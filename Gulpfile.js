3e52fda995b152ff6bd71ca980c8c94edf983c91
Mario Nava <nava.mario@gmail.com>
2015-09-26 01:43:01 -0500

    Configuramos Jshint inject y wiredep en Gulpfile.js - Instalamos con bower angular y bootstrap

---
+++ Gulpfile.js
@@ -0,0 +1,99 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    stylus = require('gulp-stylus'),
+    notify = require('gulp-notify'),
+    nib = require('nib'),
+    jshint  = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    inject  = require('gulp-inject'),
+    wiredep = require('wiredep').stream;
+
+// Servidor web de desarrollo
+gulp.task('server', function() {
+    connect.server({
+        root: 'app',
+        hostname: '0.0.0.0',
+        port: 8080,
+        livereload: true
+    });
+});
+
+// Pre-procesa archivos Stylus a CSS y recarga los cambios
+gulp.task('css', function() {
+    gulp.src('./app/stylesheets/main.styl')
+        .pipe(stylus({
+            use: nib()
+        }))
+        .pipe(gulp.dest('./app/stylesheets'))
+        .pipe(connect.reload());
+});
+
+// Recarga el navegador cuando hay cambios en el HTML
+gulp.task('html', function() {
+    gulp.src('./app/**/*.html')
+        .pipe(connect.reload());
+});
+
+// Busca errores en el JS y nos los muestra por pantalla
+gulp.task('jshint', function() {
+  return gulp.src('./app/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// para inyectarlos en el index.html
+gulp.task('inject', function() {
+  var sources = gulp.src([ './app/scripts/**/*.js', './app/stylesheets/**/*.css' ]);
+  return gulp.src('index.html', { cwd: './app' })
+    .pipe(inject(sources, {
+      read: false,
+      ignorePath: '/app'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+// Inyecta las librerias que instalemos vía Bower
+gulp.task('wiredep', function () {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({
+      directory: './app/lib'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+// Vigila cambios que se produzcan en el código
+// y lanza las tareas relacionadas
+gulp.task('watch', function(event) {
+
+    var css_watcher = gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+    css_watcher.on('change', function(event) {
+        notification('CSS', event); // Funcion que manda una notificación a la pantalla
+    });
+
+    var html_watcher = gulp.watch(['./app/**/*.html'], ['html']);
+    html_watcher.on('change', function(event) {
+        notification('HTML', event);
+    });
+
+    var jshint_watcher = gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
+    jshint_watcher.on('change', function(event) {
+        notification('JShint', event);
+    });
+
+    var wiredep_watcher = gulp.watch(['./bower.json'], ['wiredep']);
+    wiredep_watcher.on('change', function(event) {
+        notification('Bower Insertado', event)
+    });
+
+});
+
+gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+
+function notification(msg, event) {
+    return gulp.src('').pipe(notify({
+        title: msg,
+        message: 'Archivo ' + event.path + ' fue ' + event.type,
+    }));
+}
